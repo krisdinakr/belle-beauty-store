039d0e974967fd6239f037612b3f82a4904c1427
Krisdina <krssdina@gmail.com>
2024-01-17 20:18:34 +0700

    add assets image and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
+    fontFamily: {
+      inter: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      'open-sans': ['Open Sans', 'ui-serif', 'Georgia']
+    }
   },
   plugins: [],
 }
-
