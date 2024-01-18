51291cda1b07cb7c82a9514e868da09122293a40
Krisdina <krssdina@gmail.com>
2024-01-18 12:01:09 +0700

    add assets, add plugin svgr, fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'twilight-blue': '#F6F7F4',
+        'black-pearl': '#001922',
+        'sherpa-blue': '#023E55',
+      },
+    },
     fontFamily: {
       inter: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
       'open-sans': ['Open Sans', 'ui-serif', 'Georgia']
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      include: '**/*.svg?react',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
