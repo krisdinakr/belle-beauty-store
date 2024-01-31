7ee517aab1fc266aff0b4dbb1f80d7a1d93b6166
Krisdina <krssdina@gmail.com>
2024-01-31 21:28:14 +0700

    add category context

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Layout from '@/layout'
 import Routings from '@/router/Routings'
 import { AuthProvider } from '@/context/AuthContext'
+import { CategoryProvider } from '@/context/CategoryContext'
 
 const App = () => (
   <Router>
     <AuthProvider>
-      <Layout>
-        <Routings />
-      </Layout>
+      <CategoryProvider>
+        <Layout>
+          <Routings />
+        </Layout>
+      </CategoryProvider>
     </AuthProvider>
   </Router>
 )
