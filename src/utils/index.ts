57479a1d94a4ae97d10b8705261822deadb42662
Krisdina <krssdina@gmail.com>
2024-02-06 19:44:52 +0700

    add utils format currency

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,3 +9,11 @@ export function cn(...inputs: ClassValue[]) {
 export function decoded(token: string) {
   return jwtDecode<JwtPayload>(token)
 }
+
+export function formatCurrency(price: number) {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    maximumFractionDigits: 0,
+  }).format(price)
+}
