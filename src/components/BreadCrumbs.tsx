737dd1f0ae4c3d4d9bb55e572adbba6544cbbdf1
Krisdina <krssdina@gmail.com>
2024-02-24 20:30:35 +0700

    fix breadcrumbs style

--- src/components/BreadCrumbs.tsx
+++ src/components/BreadCrumbs.tsx
@@ -6,21 +6,21 @@ type BreadCrumbsProps = Pick<ICategory, '_id' | 'name' | 'slug'>
 function BreadCrumbs({ data }: { data: BreadCrumbsProps[] }) {
   return (
     <nav className="w-full">
-      <ul className="flex w-full overflow-hidden">
+      <ul className="flex w-full flex-wrap overflow-hidden">
         {data.map((i, index) => (
           <li key={i._id}>
             {index + 1 < data.length ? (
               <>
                 <Link
                   to={index === 0 ? '/' : `/category/${i.slug}`}
-                  className="text-black-pearl  transition duration-150  ease-in-out hover:text-black-pearl/80  focus:text-black-pearl/80 active:text-black-pearl/70"
+                  className="text-black-pearl transition duration-150  ease-in-out hover:text-black-pearl/80  focus:text-black-pearl/80 active:text-black-pearl/70"
                 >
                   {index === 0 ? 'Home' : i.name}
                 </Link>
                 <span className="mx-2 text-neutral-500">/</span>
               </>
             ) : (
-              <span className="truncate text-neutral-500">{i.name}</span>
+              <span className="text-neutral-500">{i.name}</span>
             )}
           </li>
         ))}
